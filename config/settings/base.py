138e246045dce610126b9e358079a6e051ea8228
kevinatahualpa <katahualpapa22@outlook.com>
2026-06-18 03:38:47 -0500

    fix: agregar whitenoise para servir estaticos en produccion

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -89,6 +90,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'apps' / 'core' / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STORAGES = {
     "default": {
